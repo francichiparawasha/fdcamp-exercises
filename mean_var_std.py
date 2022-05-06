526222522f0882acaad89497e1f3f58ebea1cb5b
Francis Chiparawasha <103685442+francichiparawasha@users.noreply.github.com>
2022-05-06 12:08:08 -0600

    Add files via upload

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,24 +1,17 @@
 
-#import numpy as np
+import numpy as np
 def calculate(x):
-    if x.ndim() < 9:
+    x = np.array(x)
+    if len(x) < 9:
         raise ValueError("List must contain nine numbers.")
-    elif x.ndim()==9:
-        print('x')
-    else:
-        print('Using only the first nine numbers')
-        x =np.x[0:8]
     
     mat = x.reshape(3,3)
 
     return{
-        'mean': [mat.mean(axis=0), mat.mean(axis=1), x.mean()],
-        'variance': [mat.var(axis=0), mat.var(axis=1), x.var()],
-        'standard deviation': [mat.std(axis=0), mat.std(axis=1), x.std()],
-        'max': [mat.max(axis=0), mat.max(axis=1), x.max()],
-        'min': [mat.min(axis=0), mat.min(axis=1), x.min()],
-        'sum': [mat.sum(axis=0), mat.sum(axis=1), x.sum()]
+        'mean': [mat.mean(axis=0).tolist(), mat.mean(axis=1).tolist(), x.mean().tolist()],
+        'variance': [mat.var(axis=0).tolist(), mat.var(axis=1).tolist(), x.var().tolist()],
+        'standard deviation': [mat.std(axis=0).tolist(), mat.std(axis=1).tolist(), x.std().tolist()],
+        'max': [mat.max(axis=0).tolist(), mat.max(axis=1).tolist(), x.max().tolist()],
+        'min': [mat.min(axis=0).tolist(), mat.min(axis=1).tolist(), x.min().tolist()],
+        'sum': [mat.sum(axis=0).tolist(), mat.sum(axis=1).tolist(), x.sum().tolist()]
         }
-
-
-calculate([0,1,2,3,4,5,6,7,8])
